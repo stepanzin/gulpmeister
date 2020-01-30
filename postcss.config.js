dd7c524344a9961d1a7e39833baf344d12af4145
Stepan Zinchenko <asworlddivide@gmail.com>
2020-01-30 22:02:38 +1000

    Remove useless prop

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    syntax: "postcss-scss",
     plugins: [
         require("postcss-easy-import")({ extensions: ".scss" }),
         require("autoprefixer")({ cascade: false }),
